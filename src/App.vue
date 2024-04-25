acdf2890fd468e68065105934bb78912f2ff1196
AlamRidha <alamrridha@gmail.com>
2024-04-25 12:26:30 +0700

    pertemuan 2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,73 @@
 <template>
-  <!-- <h1>
-    Hello World
-  </h1> -->
-  <TextH />
+  <div>
+    <h1>Declarative Rendering</h1>
+    <p>{{ namaVariabel }}</p>
+    <p>{{ namaVariabel2 }}</p>
+    <p>{{ namaVariabel3 }}</p>
+    <p>{{ variabelReactive }}</p>
+    <p>{{ variabelReactive }}</p>
 
-  <br>
+    <h1>Attribute Rendering</h1>
+    <p v-bind:class="styleParagraf">Hallo</p>
+    <h3 v-bind:class="styleParagraf">Hallo</h3>
+    <h5 v-bind:class="styleParagraf">Hallo</h5>
 
-  <ButtonC />
+    <hr>
+
+    <h1>Event Listener</h1>
+    <p>{{ data }}</p>
+    <button @click="increment">Tambah</button>
+    <button @click="decrement">Kurang</button>
+
+    <h1>Form Binding</h1>
+    <form action="">
+      <input type="text" v-model="username"><br>
+      <input type="password" v-model="password">
+    </form>
+    <p>Hasil dari form : {{ username }} dan password {{ password }}</p>
+  </div>
 </template>
 
 <!-- vue 3 -->
 <script setup>
-import ButtonC from "./components/Buttonc.vue"
-import TextH from "./components/TextHeader.vue"
+import { ref, reactive } from 'vue'
+
+const username = ref("")
+const password = ref("")
+
+
+const data = ref(0)
+
+function increment() {
+  data.value++
+}
+
+function decrement() {
+  data.value--
+}
+
+const styleParagraf = ref("paragraf")
+
+// ref bisa untuk semua tipe nilai, string,int, array, object, array object
+const namaVariabel = ref("hello vue")
+const namaVariabel2 = ref(1)
+const namaVariabel3 = ref([{
+  nama: "PBK",
+  kelas: "Praktikum"
+}])
+
+// reactive didesain untuk object, array object
+const variabelReactive = reactive({
+  nama: "PBK 2",
+  kelas: "Praktikum",
+  kode: 9
+})
 
 </script>
+
+<style scoped>
+.paragraf {
+  font-size: 25px;
+  color: red;
+}
+</style>
