29ed7e375d7c8a9aacf0c593787dfb7b8cda80fa
AlamRidha <alamrridha@gmail.com>
2024-04-26 10:36:27 +0700

    clear

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,37 @@
     <p>{{ variabelReactive }}</p>
     <p>{{ variabelReactive }}</p>
 
+    <hr style="border: 2px solid black">
+
     <h1>Attribute Rendering</h1>
     <p v-bind:class="styleParagraf">Hallo</p>
     <h3 v-bind:class="styleParagraf">Hallo</h3>
     <h5 v-bind:class="styleParagraf">Hallo</h5>
 
-    <hr>
+    <hr style="border: 2px solid black">
 
     <h1>Event Listener</h1>
     <p>{{ data }}</p>
     <button @click="increment">Tambah</button>
     <button @click="decrement">Kurang</button>
+    <button @click="sayHi">Hi</button>
+
+    <hr style="border: 2px solid black">
 
     <h1>Form Binding</h1>
     <form action="">
-      <input type="text" v-model="username"><br>
-      <input type="password" v-model="password">
+      <input type="text" v-model="username" placeholder="Tulis Username"><br>
+      <input type="password" v-model="password" placeholder="Tulis Password">
     </form>
     <p>Hasil dari form : {{ username }} dan password {{ password }}</p>
+
+    <hr style="border: 2px solid black">
+
+    <h1>Conditional Rendering</h1>
+    <p v-if="bandingData > 22">Ini nilainya Besar dari 22</p>
+    <p v-else-if="bandingData > 18 && bandingData < 22">Ini diantara 18 dan 22</p>
+    <p v-else>Ini nilainya default</p>
+
   </div>
 </template>
 
@@ -32,9 +45,10 @@
 <script setup>
 import { ref, reactive } from 'vue'
 
+const bandingData = ref(25)
+
 const username = ref("")
 const password = ref("")
-
 
 const data = ref(0)
 
@@ -46,10 +60,14 @@ function decrement() {
   data.value--
 }
 
+function sayHi() {
+  alert("Hello there")
+}
+
 const styleParagraf = ref("paragraf")
 
 // ref bisa untuk semua tipe nilai, string,int, array, object, array object
-const namaVariabel = ref("hello vue")
+const namaVariabel = ref("hello ponijbugv")
 const namaVariabel2 = ref(1)
 const namaVariabel3 = ref([{
   nama: "PBK",
